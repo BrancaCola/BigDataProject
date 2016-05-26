a42f102e55a9b85a614d23554cdf043c94aa5ae3
jg39xobe <jg39xobe@studserv.uni-leipzig.de>
2016-05-26 11:30:41 +0200

    first commit

---
+++ Pra/src/main/java/Big/Pra/App.java
@@ -0,0 +1,52 @@
+package Big.Pra;
+
+import java.util.Arrays;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.SparkConf;
+
+import scala.Tuple2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	String inputFileName = "samples/test.txt" ;
+        String outputDirName = "output" ;
+
+        SparkConf conf = new SparkConf().setAppName("org.sparkexample.WordCount").setMaster("local");
+        JavaSparkContext context = new JavaSparkContext(conf);
+
+        JavaRDD<String> file = context.textFile(inputFileName);
+        
+
+
+
+
+		JavaRDD<String> words = file.flatMap(new FlatMapFunction<String, String>() {
+		  public Iterable<String> call(String s) { return Arrays.asList(s.split(" ")); }
+		});
+		JavaPairRDD<String, Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
+		  public Tuple2<String, Integer> call(String s) { return new Tuple2<String, Integer>(s, 1); }
+		});
+		JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+		  public Integer call(Integer a, Integer b) { return a + b; }
+		});
+		counts.saveAsTextFile(outputDirName);
+
+
+
+    }
+}
+
+
