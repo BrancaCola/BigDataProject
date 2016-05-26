bed71c154a7574e96e8b1e3e1fbd2d84f9322c2c
jg39xobe <jg39xobe@studserv.uni-leipzig.de>
2016-05-26 11:40:43 +0200

    added some comments

--- Pra/src/main/java/Big/Pra/App.java
+++ Pra/src/main/java/Big/Pra/App.java
@@ -20,32 +20,27 @@ public class App
 {
     public static void main( String[] args )
     {
-    	
+    	//define path
     	String inputFileName = "samples/test.txt" ;
         String outputDirName = "output" ;
-
+        
+        //define configuration
         SparkConf conf = new SparkConf().setAppName("org.sparkexample.WordCount").setMaster("local");
         JavaSparkContext context = new JavaSparkContext(conf);
 
         JavaRDD<String> file = context.textFile(inputFileName);
-        
-
-
-
-
+        //mapper
 		JavaRDD<String> words = file.flatMap(new FlatMapFunction<String, String>() {
 		  public Iterable<String> call(String s) { return Arrays.asList(s.split(" ")); }
 		});
 		JavaPairRDD<String, Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
 		  public Tuple2<String, Integer> call(String s) { return new Tuple2<String, Integer>(s, 1); }
 		});
+		//reducer
 		JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
 		  public Integer call(Integer a, Integer b) { return a + b; }
 		});
 		counts.saveAsTextFile(outputDirName);
-
-
-
     }
 }
 
